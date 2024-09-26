8d09ccb7158bedd38cb0357c85717603168ef066
aldyrmdhns <aldyramadhans101@gmail.com>
2024-09-27 03:11:13 +0800

    Modify it to Class and change some Alert Message

--- bank_account.js
+++ bank_account.js
@@ -9,29 +9,33 @@ export class BankAccount {
             if (amount >= 5000) {
                 this.money += amount;
                 alert(`Deposit Saldo Berhasil, Saldo Saat Ini: ${this.money}`);
-                alert(`Deposit Berhasil Setelah 3 Detik!!!`);
+                alert(`Deposit Berhasil Setelah 2 Detik!!!`);
                 callback();
             } else {
                 alert("Minimal jumlah Deposit adalah Rp. 5.000");
-                alert(`Deposit Gagal Setelah 3 Detik!!!`);
+                alert(`Deposit Gagal Setelah 2 Detik!!!`);
                 callback();
             }
-        }, 3000)
+        }, 2000)
     }
 
     withdraw(amount, callback){
         setTimeout(() => {
-            if (amount >= 5000 && amount <= this.money) {
-                this.money -= amount;
-                alert(`Penarikan Saldo Berhasil, Sisa Saldo Saat Ini: ${this.money}`);
-                alert(`Penarikan Berhasil Setelah 3 Detik!!!`);
+            if (amount < 5000) {
+                alert("Minimal jumlah Penarikan adalah Rp. 5.000");
+                alert(`Penarikan Gagal Setelah 2 Detik!!!`);
+                callback();
+            } else if (amount > this.money){
+                alert("Saldo Rekening Anda Tidak Mencukupi!");
+                alert(`Penarikan Gagal Setelah 2 Detik!!!`);
                 callback();
             } else {
-                alert("Minimal jumlah Penarikan adalah Rp. 5.000");
-                alert(`Penarikan Gagal Setelah 3 Detik!!!`);
+                this.money -= amount;
+                alert(`Penarikan Saldo Berhasil, Sisa Saldo Saat Ini: ${this.money}`);
+                alert(`Penarikan Berhasil Setelah 2 Detik!!!`);
                 callback();
             }
-        }, 3000)
+        }, 2000)
     }
 
     updateDisplayMoney(element){
